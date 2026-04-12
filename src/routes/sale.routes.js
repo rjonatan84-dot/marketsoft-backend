1a774695e6d4193173a58b5716d23bc60cb46125
rjonatan84 <rjonatan84@gmail.com>
2026-04-12 11:55:32 -0500

    feat: is added the controllers

---
+++ src/routes/sale.routes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const c = require("../controllers/sale.controller");
+
+router.get("/", c.getAll);
+router.get("/:id", c.getById);
+router.post("/", c.create);
+router.put("/:id", c.update);
+router.delete("/:id", c.remove);
+
+module.exports = router;
